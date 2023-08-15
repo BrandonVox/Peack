5e46dd9791c11e7eab5176f3eeeaca0cb627d1c1
brandonvox <brandonvox9@gmail.com>
2023-08-16 06:59:07 +0700

    First Map

---
+++ Source/Peack/Peack.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Peack.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Peack, "Peack" );
---
+++ Source/Peack/PeackGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PeackGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PEACK_API APeackGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
