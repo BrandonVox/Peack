eccea132238b8d04429455da92c190218222ab0a
brandonvox <brandonvox9@gmail.com>
2023-09-02 15:52:32 +0700

    Player Controller

---
+++ Source/Peack/Public/Controller/PeackPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PeackPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PEACK_API APeackPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
