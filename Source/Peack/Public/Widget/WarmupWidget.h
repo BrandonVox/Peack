8c5d8f594ec5ffb2f598e2d847f886cb353dc236
brandonvox <brandonvox9@gmail.com>
2023-09-19 10:12:17 +0700

    Warmup Widget

---
+++ Source/Peack/Public/Widget/WarmupWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widget/CountdownWidget.h"
+#include "WarmupWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PEACK_API UWarmupWidget : public UCountdownWidget
+{
+	GENERATED_BODY()
+	
+};
