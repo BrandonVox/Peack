2742feb3b2a4fe1482eb3038737b6fc6d4483223
brandonvox <brandonvox9@gmail.com>
2023-09-19 18:53:36 +0700

    Peack Game state

---
+++ Source/Peack/Private/GameState/PeackGameState.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameState/PeackGameState.h"
+
+APeackGameState::APeackGameState()
+{
+	NetUpdateFrequency = 10.0f;
+	MinNetUpdateFrequency = 7.0f;
+}
---
+++ Source/Peack/Public/GameState/PeackGameState.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "PeackGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PEACK_API APeackGameState : public AGameState
+{
+	GENERATED_BODY()
+
+public: // Function
+	APeackGameState();
+	
+};
